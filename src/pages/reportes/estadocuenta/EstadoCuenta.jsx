67661121ac004be64629063c9fe30a6a56e0fe22
Carlos Bermudez <cbermudezg7@gmail.com>
2025-09-27 22:54:58 -0500

    reporte de estado de cuenta

--- src/pages/reportes/estadocuenta/EstadoCuenta.jsx
+++ src/pages/reportes/estadocuenta/EstadoCuenta.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { pdf } from '@react-pdf/renderer';
+import { pdf, PDFViewer } from '@react-pdf/renderer';
 import EstadoCuentaPDF from './EstadoCuentaPDF';
 import {
-  Box, Button, TextField, Typography, Grid
+  Box, Button, Typography, Grid
 } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -18,8 +18,19 @@ export default function ReporteEstadoCuenta() {
   const rutaId = useLocalStorageValue('rutaId');
 
   const generarPDF = async () => {
-    if(!desde || !hasta) return toast.error('Debe establecer una fecha de inicio y final')
-    if(desde > hasta) return toast.error('La fecha inicial debe ser mayor a la final')
+    if (!desde || !hasta) {
+      return toast.error('Debe establecer una fecha de inicio y final');
+    }
+    if (desde > hasta) {
+      return toast.error('La fecha inicial debe ser mayor a la final');
+    }
+
+    // Si hay un blob previo, liberarlo
+    if (pdfBlobUrl) {
+      URL.revokeObjectURL(pdfBlobUrl);
+      setPdfBlobUrl('');
+    }
+
     const doc = (
       <EstadoCuentaPDF
         desde={desde}
@@ -35,12 +46,12 @@ export default function ReporteEstadoCuenta() {
   return (
     <Box p={4}>
       <Grid container spacing={2}>
-        <Grid item size={3} sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <Grid item xs={3} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Typography variant="body1" gutterBottom marginBottom={3}>
             Reporte de Estado de Cuenta
           </Typography>
         </Grid>
-        <Grid item size={9} sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <Grid item xs={9} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Box display="flex" gap={2} mb={3} alignItems="center" flexWrap="wrap">
 
             <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -74,15 +85,15 @@ export default function ReporteEstadoCuenta() {
           </Box>
         </Grid>
       </Grid>
-
       {pdfBlobUrl && (
-        <iframe
-          src={pdfBlobUrl}
-          width="100%"
-          height="600px"
-          title="Reporte PDF"
-          style={{ border: '1px solid #ccc' }}
-        />
+        <PDFViewer width="100%" height="600">
+          <EstadoCuentaPDF
+            desde={desde}
+            hasta={hasta}
+            ruta={rutaId}
+            // key={`${desde}-${hasta}-${rutaId}`}
+          />
+        </PDFViewer>
       )}
     </Box>
   );
--- src/pages/reportes/estadocuenta/EstadoCuentaPDF.jsx
+++ src/pages/reportes/estadocuenta/EstadoCuentaPDF.jsx
@@ -19,6 +19,37 @@ const styles = StyleSheet.create({
   },
   tableColHeader: {
     width: "14.28%",
+    backgroundColor: "#333",
+    color:"#eee",
+    borderRightWidth: 1,
+    borderColor: "#000",
+    padding: 4,
+  },
+  tableColHeaderMov: {
+    width: "14.28%",
+    backgroundColor: "#999",
+    color:"#eee",
+    borderRightWidth: 1,
+    borderColor: "#000",
+    padding: 4,
+  },
+  tableColHeaderDes: {
+    width: "42.84%",
+    backgroundColor: "#999",
+    color:"#eee",
+    borderRightWidth: 1,
+    borderColor: "#000",
+    padding: 4,
+  },
+  tableColDes: {
+    width: "42.84%",
+    backgroundColor: "#fff",
+    borderRightWidth: 1,
+    borderColor: "#000",
+    padding: 4,
+  },
+  tableColHeaderRuta: {
+    width: "14.28%",
     backgroundColor: "#eee",
     borderRightWidth: 1,
     borderColor: "#000",
@@ -57,32 +88,8 @@ const styles = StyleSheet.create({
   },
 });
 
-// Datos de ejemplo
-// const data = [
-//   {
-//     fecha: "2025-05-03",
-//     usuario: "Juan Pérez",
-//     descripcion: "Pago de factura",
-//     naturaleza: "Crédito",
-//     cliente: 'Pedro Gomez',
-//     monto: 250.0,
-//     saldoAnterior: 1000.0,
-//     saldo: 1250.0,
-//   },
-//   {
-//     fecha: "2025-05-02",
-//     usuario: "Ana Gómez",
-//     descripcion: "Retiro de fondos",
-//     cliente: 'Pedro Gomez',
-//     naturaleza: "Débito",
-//     monto: 300.0,
-//     saldoAnterior: 1300.0,
-//     saldo: 1000.0,
-//   },
-// ];
-
 const detalles = [
-  ["ABONOS", "45,50"],
+  ["ABONOS", "45,70"],
   ["DESEMBOLSOS", "0,00"],
   ["CANTIDAD CRÉDITOS A COBRAR", "109"],
   ["CANTIDAD CRÉDITOS VISITADOS", "47"],
@@ -101,7 +108,6 @@ const totales = [
 
 export default function EstadoCuentaPDF({ desde, hasta, ruta }) {
 
-  // const [data, setData] = useState([])
   const token = localStorage.getItem('token')
   const dateDesde = dayjs(desde).format('YYYY-MM-DD');
   const dateHasta = dayjs(hasta).format('YYYY-MM-DD');
@@ -117,11 +123,9 @@ export default function EstadoCuentaPDF({ desde, hasta, ruta }) {
           }
         }
       );
-  
-      console.log(response.data);
       setData(response.data);
     } catch (error) {
-      console.error(error); // para debugging
+      console.error(error);
       toast.error(error.response?.data?.message || "Error al obtener datos");
     }
   };  
@@ -132,64 +136,195 @@ export default function EstadoCuentaPDF({ desde, hasta, ruta }) {
 
   return (
     <Document>
-    <Page size="A4" orientation="landscape" style={{ padding: 20 }}>
-      <Text style={{ fontSize: 14, marginBottom: 10 }}>Movimientos</Text>
+      <Page size="A4" orientation="landscape" style={{ padding: 20 }}>
+        <Text style={{ fontSize: 14, marginBottom: 10 }}>Movimientos</Text>
 
-      {/* Tabla */}
-      <View style={styles.table}>
-        {/* Encabezados */}
-        <View style={styles.tableRow}>
-          {["Fecha", "Usuario", "Descripción", "Cliente", "Naturaleza", "Monto", "Saldo", "Saldo Anterior"].map((col, index) => (
-            <View
-              key={index}
-              style={[
-                styles.tableColHeader,
-                index === 7 && styles.lastCol,
-              ]}
-            >
-              <Text style={styles.tableCell}>{col}</Text>
+        {/* Tabla */}
+        <View style={styles.table}>
+          {/* Filas de datos */}
+          {data.map((item, index) => {
+            return(
+              <>
+                <View style={styles.tableRow}>
+                  {["Ruta", "Fecha Apertura", "Fecha Cierre", "Saldo Inicial", "Saldo Final", "Apertura por:", "Cierre por:"].map((col, index) => (
+                    <View
+                      key={index}
+                      style={[
+                        styles.tableColHeader,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{col}</Text>
+                    </View>
+                  ))}
+                </View>
+                <View style={styles.tableRow} key={index}>
+                    <View
+                      style={[
+                        styles.tableColHeaderRuta,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{item.ruta_nombre}</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderRuta,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{dayjs(item.fecha_apertura).format("DD/MM/YYYY")}</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderRuta,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{dayjs(item.fecha_cierrr).format("DD/MM/YYYY")}</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderRuta,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{item.monto_inicial}</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderRuta,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{item.monto_final}</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderRuta,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{item.usuario_open}</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderRuta,
+                        index === 7 && styles.lastCol,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>{item.usuario_close}</Text>
+                    </View>
+                </View>
+                <View style={styles.tableRow}>
+                    <View
+                      style={[
+                        styles.tableColHeaderMov
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>Tipo</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderMov
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>Fecha</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderDes,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>Descripción</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderMov,
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>Monto</Text>
+                    </View>
+                    <View
+                      style={[
+                        styles.tableColHeaderMov,
+                        styles.lastCol
+                      ]}
+                    >
+                      <Text style={styles.tableCell}>Usuario</Text>
+                    </View>
+                </View>
+                {
+                  item.movimientos.map((mov, movIndex)=>{
+                    return(
+                      <>
+                        <View style={styles.tableRow} key={movIndex}>
+                          <View
+                            style={[
+                              styles.tableCol,
+                            ]}
+                          >
+                            <Text style={styles.tableCell}>{mov.tipo}</Text>
+                          </View>
+                          <View
+                            style={[
+                              styles.tableCol,
+                            ]}
+                          >
+                            <Text style={styles.tableCell}>{dayjs(mov.createdAt).format("DD/MM/YYYY")}</Text>
+                          </View>
+                          <View
+                            style={[
+                              styles.tableCol,
+                              styles.tableColDes
+                            ]}
+                          >
+                            <Text style={styles.tableCell}>{mov.descripcion}</Text>
+                          </View>
+                          <View
+                            style={[
+                              styles.tableCol
+                            ]}
+                          >
+                            <Text style={styles.tableCell}>{mov.monto}</Text>
+                          </View>
+                          <View
+                            style={[
+                              styles.tableCol
+                            ]}
+                          >
+                            <Text style={styles.tableCell}>{mov.usuario_nombre}</Text>
+                          </View>
+                        </View>
+                      </>
+                    )
+                  })
+                }
+              </>
+            )
+          })}
+        </View>
+
+        {/* <Text style={{ fontSize: 14, marginBottom: 10, marginTop: 10 }}>Resumen del Día</Text>
+        <View style={styles.table}>
+          {detalles.map(([key, value], idx) => (
+            <View style={styles.row} key={idx}>
+              <Text style={styles.cellKey}>{key}</Text>
+              <Text style={styles.cellValue}>{value}</Text>
             </View>
           ))}
         </View>
 
-        {/* Filas de datos */}
-        {/* {data.map((item, rowIndex) => (
-          <View style={styles.tableRow} key={rowIndex}>
-            {[item.fecha, item.usuario, item.descripcion, item.cliente, item.naturaleza, `$${item.monto.toFixed(2)}`, `$${item.saldo.toFixed(2)}`, `$${item.saldoAnterior.toFixed(2)}`].map((val, colIndex) => (
-              <View
-                key={colIndex}
-                style={[
-                  styles.tableCol,
-                  colIndex === 7 && styles.lastCol,
-                ]}
-              >
-                <Text style={styles.tableCell}>{val}</Text>
-              </View>
-            ))}
-          </View>
-        ))} */}
-      </View>
-
-      <Text style={{ fontSize: 14, marginBottom: 10, marginTop: 10 }}>Resumen del Día</Text>
-      <View style={styles.table}>
-        {detalles.map(([key, value], idx) => (
-          <View style={styles.row} key={idx}>
-            <Text style={styles.cellKey}>{key}</Text>
-            <Text style={styles.cellValue}>{value}</Text>
-          </View>
-        ))}
-      </View>
-
-      <Text style={{ fontSize: 14, marginBottom: 10, marginTop: 10 }}>Totales</Text>
-      <View style={styles.table}>
-        {/* {totales.map(([key, value], idx) => (
-          <View style={styles.row} key={idx}>
-            <Text style={styles.cellKey}>{key}</Text>
-            <Text style={styles.cellValue}>{value}</Text>
-          </View>
-        ))} */}
-      </View>
-    </Page>
-  </Document>
+        <Text style={{ fontSize: 14, marginBottom: 10, marginTop: 10 }}>Totales</Text>
+        <View style={styles.table}>
+          {totales.map(([key, value], idx) => (
+            <View style={styles.row} key={idx}>
+              <Text style={styles.cellKey}>{key}</Text>
+              <Text style={styles.cellValue}>{value}</Text>
+            </View>
+          ))}
+        </View> */}
+      </Page>
+    </Document>
   );
 }
